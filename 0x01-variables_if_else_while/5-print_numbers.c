5dec1c05aece6e2d7a392653c25f1291d6b2a3d4
HendrixTech <hendrixobuks@gmial.com>
2022-09-09 18:45:08 -0700

    C programming variables

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - Prints the numbers since 0 to 9
- *
- * Return: Always (Success)
- */
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	char n;
@@ -14,6 +15,6 @@ int main(void)
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 }
