cf8b734491d6781c18749cc233e515ad4cb17bdb
HendrixTech <hendrixobuks@gmial.com>
2022-10-04 19:29:35 -0700

    corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,23 +10,23 @@
 
 char *str_concat(char *s1, char *s2)
 {
-        char *concat;
-        int i, ci;
+	char *concat;
+	int i, ci;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        i = ci = 0;
-        while (s1[i] != '\0')
-                i++;
-        while (s2[ci] != '\0')
-                ci++;
-        concat = malloc(sizeof(char) * (i + ci + 1));
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	concat = malloc(sizeof(char) * (i + ci + 1));
 
-        if (concat == NULL)
-                return (NULL);
+	if (concat == NULL)
+		return (NULL);
 	i = ci =0;
 	while (s1[i] != '\0')
 	{
