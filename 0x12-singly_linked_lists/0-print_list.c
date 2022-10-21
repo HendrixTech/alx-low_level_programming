87804f21579cd834f1312fabbc22d3f6589603f1
HendrixTech <hendrixobuks@gmial.com>
2022-10-20 19:27:17 -0700

    singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,23 @@
+#include <stdio.h>
 #include "lists.h"
-
-
 /**
- * print_list - function that prints all the elements of a list_t list
- * @h: list type
- * Return: the number of nodes
- */
-
-
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list
+ * Return: number of nodes printed
+**/
 size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	size_t n = 0;
 
 
-	if (!h)
-		return (0);
 	while (h)
 	{
-		if (h->str)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (!h->str)
 			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		size++;
+		n++;
 	}
-	return (size);
+	return (n);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,18 @@
 #include "lists.h"
-
-
 /**
- * list_len - function that return the number of elements of a list_t list
- * @h: list type
- * Return: the number of nodes
- */
-
-
+ * list_len - Calculate the number of elements
+ * @h: Pointer to a list
+ * Return: Integer
+ **/
 size_t list_len(const list_t *h)
 {
-	size_t size = 0;
-
-
-	if (!h)
-		return (0);
-	while (h)
+	const list_t *temp;
+	unsigned int counter = 0;
+	temp = h;
+	while (temp)
 	{
-		h = h->next;
-		size++;
+		counter++;
+		temp = temp->next;
 	}
-	return (size);
+	return (counter);
 }
