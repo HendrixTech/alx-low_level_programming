d080405d416abc9a873da928a61a7f13752cb38c
HendrixTech <hendrixobuks@gmail.com>
2023-06-19 15:06:14 -0700

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of dog
+ *
+ * @age: Age of dog
+ *
+ * @owner: Dog's owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
