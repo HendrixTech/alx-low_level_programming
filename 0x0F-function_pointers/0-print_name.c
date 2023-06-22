65dd555dba51218153e264df61d843f1a8de038f
HendrixTech <hendrixobuks@gmail.com>
2023-06-21 18:35:52 -0700

    re debug

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -9,7 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (!name || !f)
 	{
 		return;
 	}
