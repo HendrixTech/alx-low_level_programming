f98a56321a4b633bf4a9c801e2286f6b29d44c48
HendrixTech <hendrixobuks@gmail.com>
2023-06-23 07:08:06 -0700

    debug

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,7 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always o.
- */
-
-/**
- * __attribute__((__unused__)) - This is a compiler-specific attribute that
- * tells the compiler to ignore any warnings about the variable argc being
- * unused
+ * Return: Always 0
  */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - adds positive numbers
- * @argc: Argument counter
- * @argv: An array of poniters to characters
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 and 1
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
